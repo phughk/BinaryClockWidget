8b04db6fecbdea6aa0fdf546648dcfaa94566ea0
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2014-04-25 09:20:53 +0100

    Screen draw

--- src/com/phughk/binaryclock/widget/BinaryClockAppWidgetProvider.java
+++ src/com/phughk/binaryclock/widget/BinaryClockAppWidgetProvider.java
@@ -9,6 +9,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.util.Log;
 import android.view.SurfaceView;
 import android.webkit.WebView.FindListener;
 import android.widget.RemoteViews;
@@ -19,7 +20,7 @@ public class BinaryClockAppWidgetProvider extends AppWidgetProvider
 	// private SimpleDateFormat formatter = new
 	// SimpleDateFormat("dd MMM yyyy  hh:mm:ss a");
 	String strWidgetText = "";
-    Bitmap bmp = Bitmap.createBitmap(100, 100, Bitmap.Config.RGB_565);
+    Bitmap bmp = Bitmap.createBitmap(400, 300, Bitmap.Config.RGB_565);
 
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds)
@@ -52,17 +53,22 @@ public class BinaryClockAppWidgetProvider extends AppWidgetProvider
         //Do drawing
         Canvas c = new Canvas(bmp);
         Paint paint = new Paint();
-        paint.setColor(Color.CYAN);
-        paint.setStyle(Style.FILL);
-        c.drawText("works", 100.0f, 100.0f, paint);
-        c.drawPaint(paint);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(1);
+//        paint.setStyle(Style.FILL);
+//        c.drawText("works", 100.0f, 100.0f, paint);
+//        c.drawPaint(paint);
+        
+        c.drawRect(0, 0, 400, 300, paint);
+        paint.setColor(Color.BLUE);
+        c.drawRect(10, 10, 390, 290, paint);
         
 		for (int appWidgetID: appWidgetIds)
 		{
 			RemoteViews updateViews = new RemoteViews(context.getPackageName(), com.phughk.binaryclock.R.layout.binary_clock_widget_layout);
 	        updateViews.setTextViewText(com.phughk.binaryclock.R.id.title, "it works");
 	        updateViews.setImageViewBitmap(com.phughk.binaryclock.R.id.clockImage, bmp);
-
+	        
 	        appWidgetManager.updateAppWidget(appWidgetID, updateViews);
 		}
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
